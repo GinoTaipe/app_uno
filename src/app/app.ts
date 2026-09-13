f94db559af81811537668d24b7390f6ced935ce7
GinoTaipe <gino_gjtm@hotmail.com>
2026-09-13 16:05:21 -0500

    despliegue

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Formulario } from './components/formulario/formulario';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Formulario],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
